1743e019bd8329b328ddd281bc1c41e2b4f3a322
Nara Moon <miwata.natsuki.infratop@gmail.com>
2019-03-26 12:47:34 +0900

    Finish -6chapter

---
+++ lesson3.rb
@@ -0,0 +1,16 @@
+puts "webcamp".upcase
+name = "Natsuki" 
+puts name
+
+name = "1"
+puts "こんにちは" + name
+
+names = ["Perl", "Ruby", "Python", "Swift"]
+puts names[1] + names[3]
+
+
+tall = {:太郎=>185, "次郎"=>170}
+puts tall[:太郎]
+
+subjects = ["国語", "算数", "理科", "社会"]
+puts "こんにちは" + subjects[2]
